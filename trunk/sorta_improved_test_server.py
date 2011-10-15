368d7fb715f8a3207b69d6347f92b84c88ff4bba
andrew@jabagawee.com <andrew@jabagawee.com@737709cc-4f13-c9ad-6d7a-a9c11883d90d>
2011-10-15 06:28:20 +0000

    stupid peter

--- trunk/sorta_improved_test_server.py
+++ trunk/sorta_improved_test_server.py
@@ -1,13 +1,14 @@
-from xmlrpc.server import SimpleXMLRPCServer
-from xmlrpc.server import SimpleXMLRPCRequestHandler
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 import random
+import sys
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
 # Create server
-server = SimpleXMLRPCServer(("172.16.67.193", 8080), requestHandler=RequestHandler)
+server = SimpleXMLRPCServer(("172.16.73.133", int(sys.argv[1])), requestHandler=RequestHandler)
 server.register_introspection_functions()
 
 # Variables to store state
@@ -23,13 +24,13 @@ def ping(x):
 
 # half locked
 def start_game(s):
-    print("New game started.")
+    print "New game started."
     global top_discard
     top_discard = s['initial_discard']
-    print("Top discard card: %s" %(top_discard))
+    print "Top discard card: %s" %(top_discard)
     if s['player_id'] == 0:
         game_just_started = True
-    print("We are player %s." %(0 if game_just_started else 1))
+    print "We are player %s." %(0 if game_just_started else 1)
     return ''
 
 def get_move(s):
@@ -39,32 +40,49 @@ def get_move(s):
     if game_just_started:
         game_just_started = False
         return {'move' : 'request_discard', 'idx' : random.randint(0, 19)}
-    if turn_total>60:
-        outlier=find_first_outlier()
-        if outlier>0:
+    if turn_total>40:
+        outlier=find_first_outlier(s)
+        if outlier >= 0:
             return {'move' : 'request_discard', 'idx' : outlier}
+        else:
+            d, r = s['discard'], s['rack']
+            if d-1 in r and r.index(d-1) < 19:
+                return {'move' : 'request_discard', 'idx' : r.index(d-1) + 1}
+            if d-2 in r and r.index(d-2) < 18:
+                return {'move' : 'request_discard', 'idx' : r.index(d-2) + 2}
+            else:
+                return {'move' : 'request_deck'}
     else:
         return {'move' : 'request_discard', 'idx' : (s['discard']-1)/4}
-    pass
+
 def find_first_outlier(s):
     rack=s['rack']
     n=1
     while n<19:
-        if(rack[n]<rack[n+1] or rack[n]<rack[n-1]):
+        if rack[n] < rack[n-1]:
             return n
+        n += 1
     return -1
+
 def get_deck_exchange(s):
-    pass
+    d, r = s['card'], s['rack']
+    if d-1 in r and r.index(d-1) < 19:
+        return r.index(d-1) + 1
+    if d-2 in r and r.index(d-2) < 18:
+        return r.index(d-2) + 2
+    else:
+        print "we're fucked"
+        return "we're fucked"
 
 # locked
 def move_result(s):
-    print("Move result: %s" %(s['move']))
+    print "Move result: %s" %(s['move'])
     return ''
 
 # locked
 def game_result(s):
-    print("Game over because %s.\nYour score:  %s\nTheir score: %s" %(s['reason'], s['your_score'], s['other_score']))
-    print()
+    print "Game over because %s.\nYour score:  %s\nTheir score: %s" %(s['reason'], s['your_score'], s['other_score'])
+    print
     return ''
 
 # functions that are defined in gamelogic.py
--- trunk/test_server.py
+++ trunk/test_server.py
@@ -1,13 +1,14 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 import random
+import sys
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
 # Create server
-server = SimpleXMLRPCServer(("172.16.73.133", 8080), requestHandler=RequestHandler)
+server = SimpleXMLRPCServer(("172.16.73.133", int(sys.argv[1])), requestHandler=RequestHandler)
 server.register_introspection_functions()
 
 # Variables to store state
@@ -22,35 +23,60 @@ def ping(x):
 
 # half locked
 def start_game(s):
-    print "New game started."
+    print "New game started.\n"
     global top_discard
     top_discard = s['initial_discard']
-    print "Top discard card: %s" %(top_discard)
+    print "Top discard card: %s.\n" %(top_discard)
     if s['player_id'] == 0:
         game_just_started = True
-    print "We are player %s." %(0 if game_just_started else 1)
+    print "We are player %s.\n" %(0 if game_just_started else 1)
     return ''
 
 def get_move(s):
+    # return {'move' : 'request_discard', 'idx' : -1}
     global game_just_started
     if game_just_started:
         game_just_started = False
         return {'move' : 'request_discard', 'idx' : random.randint(0, 19)}
-    return {'move' : 'request_discard', 'idx' : (s['discard']-1)/4}
-    pass
+    d = s['discard']
+    r = s['rack']
+    if d - 1 in r and r.index(d - 1) < 19 and abs((r.index(d - 1) + 1) * 4 - d) < 3:
+        print 'request_discard @ %s' %(r.index(d-1)+1)
+        return {'move' : 'request_discard', 'idx' : r.index(d-1) + 1}
+    elif d - 2 in r and r.index(d - 2) < 18 and abs((r.indx(d - 1) + 1) * 4 - d) < 3:
+        print 'request_discard @ %s' %(r.index(d-2)+2)
+        return {'move' : 'request_discard', 'idx' : r.index(d-2) + 2}
+    # elif d - 3 in r and r.index(d - 3) < 17:
+    #     return {'move' : 'request_discard', 'idx' : r.index(d-3) + 3}
+    else:
+        print 'going to request_deck'
+        return {'move' : 'request_deck'}
 
 def get_deck_exchange(s):
-    pass
+    r = s['rack'] # [int] * 20
+    c = s['card'] # int
+    if c - 1 in r and r.index(c - 1) < 19 and abs((r.index(c - 1) + 1) * 4 - c) < 3:
+        print 'request_deck @ %s' %(r.index(c-1)+1)
+        return r.index(c - 1) + 1
+    elif c - 2 in r and r.index(c - 2) < 18 and abs((r.index(c - 1) + 1) * 4 - c) < 3:
+        print 'request_deck @ %s' %(r.index(c-2)+2)
+        return r.index(c - 2) + 2
+    # elif c - 3 in r and r.index(c - 3) < 17:
+    #     return r.index(c - 3) + 3
+    else:
+        print 'request_deck @ %s' %((c-1)/4)
+        return (c - 1) / 4
 
 # locked
 def move_result(s):
-    print "Move result: %s" %(s['move'])
+    print "Move result: %s.\n" %(s['move'])
+    if s['move'] == 'move_ended_game':
+        print s['reason']
     return ''
 
 # locked
 def game_result(s):
-    print "Game over because %s.\nYour score:  %s\nTheir score: %s" %(s['reason'], s['your_score'], s['other_score'])
-    print
+    print "Game over because %s.\nYour score:  %s\nTheir score: %s\n\n" %(s['reason'], s['your_score'], s['other_score'])
     return ''
 
 # functions that are defined in gamelogic.py
