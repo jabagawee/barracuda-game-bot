3a5bb5c043257a7501b7ae1d280b2c90538fc4f8
andrew@jabagawee.com <andrew@jabagawee.com@737709cc-4f13-c9ad-6d7a-a9c11883d90d>
2011-10-15 01:14:05 +0000

    have a client and a server :D

--- trunk/gamelogic.py
+++ trunk/gamelogic.py
@@ -1,3 +1,23 @@
 # /usr/bin/env python
 
 # to the pain
+def ping(x):
+    return 'pong' if x == 'ping' else 'what in the fuck?'
+
+def start_game(stuff):
+    raise NotImplementedError
+
+def get_move(stuff):
+    raise NotImplementedError
+
+def get_deck_exchange(stuff):
+    raise NotImplementedError
+
+def move_result(stuff):
+    raise NotImplementedError
+
+def move_game(stuff):
+    raise NotImplementedError
+
+def game_result(stuff):
+    raise NotImplementedError
