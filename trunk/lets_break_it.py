ebc2dce7c5d357169c2270b312833c733795bf03
andrew@jabagawee.com <andrew@jabagawee.com@737709cc-4f13-c9ad-6d7a-a9c11883d90d>
2011-10-15 16:38:15 +0000

    yay source control?

--- trunk/lets_break_it.py
+++ trunk/lets_break_it.py
@@ -27,7 +27,7 @@ turnNum = 0
 
 notTurnNumed = True
 
-iLoveCheating = False
+focus = None
 
 def ping(x):
     return 'pong' if x == 'ping' else 'fuck you'
@@ -46,6 +46,9 @@ def get_move(s):
     global partOfRunLength
     global distanceFromOptimal
     turnNum += 1
+
+    # if turnNum <= sys.argv[3]:
+     #   return makeMove('request_deck', 0, s['rack'])
 
     if s['other_player_moves'] != [] and s['other_player_moves'][0][1]['move'] == 'take_deck':
         deckSize -= 1
@@ -66,7 +69,7 @@ def get_move(s):
     if s['discard'] in opponentKnownCards:
         opponentKnownCards.remove(s['discard'])
     
-    partOfRunLength = [0] * 20
+    partOfRunLength = [1] * 20
     lastValue = s['rack'][0]
     runLength = 1
     for i in xrange(1, 20):
@@ -80,6 +83,31 @@ def get_move(s):
             runLength = 1
         lastValue = s['rack'][i]
 
+
+    # if max(partOfRunLength) >= 5:
+        # new mode
+
+    '''
+    for i in xrange(20):
+        print 'loop %s' %(i)
+        print partOfRunLength
+        right_adjacent, left_adjacent = 20, 20
+        if partOfRunLength[i] > 1:
+            if i + partOfRunLength[i] < 20:
+                right_adjacent = partOfRunLength[i+partOfRunLength[i]]
+            print 'right_adjacent: %s' %(right_adjacent)
+            j = i - 1
+            while j >= 0 and partOfRunLength[j] == 0:
+                j -= 1
+            left_adjacent =  partOfRunLength[j]
+            if partOfRunLength[i] > right_adjacent:
+                for k in xrange(right_adjacent):
+                    partOfRunLength[i+partOfRunLength[i]+k] = 1
+            if partOfRunLength[i] > left_adjacent:
+                for k in xrange(j):
+                    partOfRunLength[i-k] = 1
+    '''
+
     totalDistance = 0
     distanceFromOptimal = [0] * 20
     for i in xrange(20):
@@ -87,36 +115,49 @@ def get_move(s):
         totalDistance += distanceFromOptimal[i]
 
     rlFound = 1
-    if isInOrder(s['rack']):
+    # if isInOrder(s['rack']):
+    if countRedSquares(s['rack']) < int(sys.argv[2]):
+        if focus != None:
+            if s['discard'] == s['rack'][focus] - 1 and focus > 0:
+                if inRunLength(partOfRunLength, focus - 1) <= partOfRunLength[focus]:
+                    return makeMove('request_discard', focus - 1, s['rack'])
+            if s['discard'] == s['rack'][focus + partOfRunLength[focus] - 1] + 1 and focus + partOfRunLength[focus] < 20:
+                if inRunLength(partOfRunLength, focus + partOfRunLength[focus]) <= partOfRunLength[focus]:
+                    return makeMove('request_discard', focus + partOfRunLength[focus], s['rack'])
+
         for i in xrange(20):
-            if partOfRunLength[i] > 1:
+            if partOfRunLength[i] > 0:
                 rlFound = max(rlFound, partOfRunLength[i])
+                thing = True
+                '''
+                if partOfRunLength[i] == 1:
+                    for x in xrange(-5, 6):
+                        if i+x < 0 or i+x>19 or x == 0:
+                            continue
+                        if partOfRunLength[i+x] > 1:
+                            thing = False
+
+                    for x in xrange(-2, 3):
+                        if i+x < 0 or i+x > 19 or x == 0:
+                            continue
+                        if inRunLength(partOfRunLength, i+x) > 1:
+                            thing = False
+                '''
+
+
                 if s['discard'] == s['rack'][i] - 1 and i > 0:
-                    new_rack = s['rack'][:]
-                    new_rack[i - 1] = s['discard']
-                    if isInOrder(new_rack):
+                    if thing and inRunLength(partOfRunLength, i - 1) <= partOfRunLength[i]:
                         return makeMove('request_discard', i - 1, s['rack'])
                 if s['discard'] == s['rack'][i + partOfRunLength[i] - 1] + 1 and i + partOfRunLength[i] < 20:
-                    return makeMove('request_discard', i + partOfRunLength[i], s['rack'])
+                    if thing and inRunLength(partOfRunLength, i + partOfRunLength[i]) <= partOfRunLength[i]:
+                        return makeMove('request_discard', i + partOfRunLength[i], s['rack'])
 
-        if rlFound > 2:
-            print 'NOPEOLEOFOMGWTFBBQ'
-
-        if rlFound > 1 and turnNum > 70 - rlFound * 3 and iLoveCheating:
-            time.sleep(s['remaining_microseconds']/1000000 + 1.5)
-            print "ready for next game"
-
-        if rlFound > 1 and turnNum > 55:
+        if rlFound > 1: # and turnNum > 25:
             for i in xrange(20):
                 new_rack = s['rack'][:]
                 new_rack[i] = s['discard']
-                if isInOrder(new_rack) and inRunLength(partOfRunLength, i) == -1:
-                    print "safe to make a discard, going for it GOTO"
-                    print "replacing %s with %s" %(s['rack'][i], s['discard'])
+                if isInOrder(new_rack) and inRunLength(partOfRunLength, i) < 2:
                     return makeMove('request_discard', i, s['rack'])
-            print "unsafe ever to make a replacement"
-            print "see rack and discard as follows"
-            print "rack: %s\ndiscard: %s" %(s['rack'], s['discard'])
 
         return makeMove('request_deck', 0, s['rack'])
         
@@ -137,21 +178,72 @@ def makeMove(move, idx, rack):
     
 def get_deck_exchange(s):
     global partOfRunLength
+    global focus
 
-    if isInOrder(s['rack']):
+    # if turnNum <= sys.argv[3]:
+    #     return deckExchange((s['card']-1)/4, s['rack'])
+
+    rlFound = 1
+
+    if countRedSquares(s['rack']) < int(sys.argv[2]):
+        if focus != None:
+            if s['card'] == s['rack'][focus] - 1 and focus > 0:
+                if inRunLength(partOfRunLength, focus - 1) <= partOfRunLength[focus]:
+                    return deckExchange(focus - 1, s['rack'])
+            if s['card'] == s['rack'][focus + partOfRunLength[focus] - 1] + 1 and focus + partOfRunLength[focus] < 20:
+                if inRunLength(partOfRunLength, focus + partOfRunLength[focus]) <= partOfRunLength[focus]:
+                    return deckExchange(focus + partOfRunLength[focus], s['rack'])
         for i in xrange(20):
-            if partOfRunLength[i] > 1:
+            if partOfRunLength[i] > 0:
+                thing = True
+                '''
+                if partOfRunLength[i] == 1:
+                    for x in xrange(-5, 6):
+                        if i+x < 0 or i+x>19 or x == 0:
+                            continue
+                        if partOfRunLength[i+x] > 1:
+                            thing = False
+
+                    for x in xrange(-2, 3):
+                        if i+x < 0 or i+x > 19 or x == 0:
+                            continue
+                        if inRunLength(partOfRunLength, i+x) > 1:
+                            thing = False
+                '''
+
+                rlFound = max(rlFound, partOfRunLength[i])
                 if s['card'] == s['rack'][i] - 1 and i > 0:
-                    return deckExchange(i - 1, s['rack'])
+                    if thing and inRunLength(partOfRunLength, i - 1) <= partOfRunLength[i]:
+                        return deckExchange(i - 1, s['rack'])
                 if s['card'] == s['rack'][i + partOfRunLength[i] - 1] + 1 and i + partOfRunLength[i] < 20:
-                    return deckExchange(i + partOfRunLength[i], s['rack'])
+                    if thing and inRunLength(partOfRunLength, i + partOfRunLength[i]) <= partOfRunLength[i]:
+                        return deckExchange(i + partOfRunLength[i], s['rack'])
 
     for i in xrange(20):
         new_rack = s['rack'][:]
         new_rack[i] = s['card']
-        if isInOrder(new_rack) and inRunLength(partOfRunLength, i) == -1:
+        if isInOrder(new_rack) and inRunLength(partOfRunLength, i) < 2:
             return deckExchange(i, s['rack'])
 
+    for i in xrange(20):
+        new_rack = s['rack'][:]
+        new_rack[i] = s['card']
+        if countRedSquares(new_rack) == countRedSquares(s['rack']) and inRunLength(partOfRunLength, i) < 2:
+            return deckExchange(i, s['rack'])
+
+    for i in xrange(20):
+        new_rack = s['rack'][:]
+        new_rack[i] = s['card']
+        if isInOrder(new_rack):
+            return deckExchange(i, s['rack'])
+
+    for i in xrange(20):
+        new_rack = s['rack'][:]
+        new_rack[i] = s['card']
+        if countRedSquares(new_rack) == countRedSquares(s['rack']):
+            return deckExchange(i, s['rack'])
+
+    print "OHMYGOD YOU SHOULD NEVER BE HERE WTFFFFFFFFFF"
     return deckExchange((s['card']) / 4, s['rack'])
 
 def deckExchange(index, rack):
@@ -161,15 +253,15 @@ def deckExchange(index, rack):
     return index
 
 def inQuadrant(num, position):
-    upperBound = (num - 1) / 4 + 3
-    lowerBound = (num - 1) / 4 - 3
+    upperBound = (num - 1) / 4 + 4
+    lowerBound = (num - 1) / 4 - 4
     return position < upperBound and position > lowerBound
     
 def inRunLength(runLengths, index):
     for i in xrange(len(runLengths)):
         if runLengths[i] > 0:
             if index >= i and index < i + runLengths[i]:
-                return i
+                return runLengths[i]
     return -1
 
 def getPossibleCardsInDeck(myCards):
@@ -189,11 +281,30 @@ def isInOrder(rack):
         lastValue = rack[i]
     return True
 
+def countRedSquares(rack):
+    a = 0
+    lastValue = rack[0]
+    for i in xrange(1, len(rack)):
+        if rack[i] < lastValue:
+            a += 1
+        lastValue = rack[i]
+    return a
+
+def distance(rack):
+    totalDistance = 0
+    for i in xrange(20):
+        totalDistance += abs(i - (rack[i] - 1) / 4)
+    return totalDistance
+
+def h(rack):
+    return 50 * countRedSquares(rack) - distance(rack)
+
 def move_result(s):
     print "Move result: %s." %(s['move'])
     return ''
 
 def game_result(s):
+    print 'turnNum: %s' %(turnNum)
     print "Game over because: %s.\nOur score: %s\nTheir score: %s\n\n" %(s['reason'], s['your_score'], s['other_score'])
     return ''
 
