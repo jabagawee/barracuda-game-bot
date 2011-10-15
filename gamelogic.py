6bf24e806606fee347017b02f253bdf0e90ad8d6
andrew@jabagawee.com <andrew@jabagawee.com@737709cc-4f13-c9ad-6d7a-a9c11883d90d>
2011-10-15 01:06:34 +0000

    run the game

---
+++ gamelogic.py
@@ -0,0 +1,3 @@
+# /usr/bin/env python
+
+# to the pain
--- test_server.py
+++ test_server.py
@@ -1,5 +1,6 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+from gamelogic import *
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
@@ -9,22 +10,13 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
 server = SimpleXMLRPCServer(("localhost", 8000), requestHandler=RequestHandler)
 server.register_introspection_functions()
 
-# Register pow() function; this will use the value of
-# pow.__name__ as the name, which is just 'pow'.
-server.register_function(pow)
-
-# Register a function under a different name
-def adder_function(x,y):
-    return x + y
-server.register_function(adder_function, 'add')
-
-# Register an instance; all the methods of the instance are
-# published as XML-RPC methods (in this case, just 'div').
-class MyFuncs:
-    def div(self, x, y):
-        return x // y
-
-server.register_instance(MyFuncs())
+# functions that are defined in gamelogic.py
+server.register_function(ping)
+server.register_function(start_game)
+server.register_function(get_move)
+server.register_function(get_deck_exchange)
+server.register_function(move_result)
+server.register_function(game_result)
 
 # Run the server's main loop
 server.serve_forever()
