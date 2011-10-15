f0ffa9c23cadaeb0932627582f088f591eadbae5
andrew@jabagawee.com <andrew@jabagawee.com@737709cc-4f13-c9ad-6d7a-a9c11883d90d>
2011-10-15 02:46:11 +0000

    we're winnning bitches

--- trunk/test_server.py
+++ trunk/test_server.py
@@ -1,14 +1,57 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
-from gamelogic import *
+import random
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
 # Create server
-server = SimpleXMLRPCServer(("localhost", 8000), requestHandler=RequestHandler)
+server = SimpleXMLRPCServer(("172.16.73.133", 8080), requestHandler=RequestHandler)
 server.register_introspection_functions()
+
+# Variables to store state
+top_discard, not_top_discard = 0, set()
+opponent_range = set(range(1,80+1))
+opponent_rack = [0] * 20
+game_just_started = False
+
+# locked
+def ping(x):
+    return 'pong' if x == 'ping' else 'what in the fuck'
+
+# half locked
+def start_game(s):
+    print "New game started."
+    global top_discard
+    top_discard = s['initial_discard']
+    print "Top discard card: %s" %(top_discard)
+    if s['player_id'] == 0:
+        game_just_started = True
+    print "We are player %s." %(0 if game_just_started else 1)
+    return ''
+
+def get_move(s):
+    global game_just_started
+    if game_just_started:
+        game_just_started = False
+        return {'move' : 'request_discard', 'idx' : random.randint(0, 19)}
+    return {'move' : 'request_discard', 'idx' : s['discard']/4}
+    pass
+
+def get_deck_exchange(s):
+    pass
+
+# locked
+def move_result(s):
+    print "Move result: %s" %(s['move'])
+    return ''
+
+# locked
+def game_result(s):
+    print "Game over because %s.\nYour score:  %s\nTheir score: %s" %(s['reason'], s['your_score'], s['other_score'])
+    print
+    return ''
 
 # functions that are defined in gamelogic.py
 server.register_function(ping)
